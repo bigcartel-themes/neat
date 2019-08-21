4a845de04f90ed5185bf38ca2f47a83dd5f86c0e
Nick Endle <nick.endle@gmail.com>
2019-08-21 16:14:05 -0500

    Break JS into a separate file and add new POG code

---
+++ source/javascripts/store.js
@@ -0,0 +1,253 @@
+function isIE() {
+  var rv = -1;
+  if (navigator.appName == 'Microsoft Internet Explorer') {
+    var ua = navigator.userAgent;
+    var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null) rv = parseFloat(RegExp.$1);
+  }
+  else if (navigator.appName == 'Netscape') {
+    var ua = navigator.userAgent;
+    var re = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null) rv = parseFloat(RegExp.$1);
+  }
+  return rv == -1 ? false : true;
+}
+$(document).ready(function () {
+
+  $('.image_gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image </a> could not be loaded.',
+    }
+  });
+
+  if ($('.featured').length) {
+    var waypoint = new Waypoint({
+      element: $('.featured'),
+      handler: function(direction) {
+        var screenWidth = Waypoint.viewportWidth();
+        if (screenWidth > 800) {
+          if (direction === 'down') {
+            $('.featured').animate({ opacity: 0 });
+          }
+          else {
+            $('.featured').animate({ opacity: 1 });
+          }
+        }
+      },
+      offset: 120
+    });
+  }
+  if ($('.content').length) {
+    var waypoint = new Waypoint({
+      element: $('.content'),
+      handler: function(direction) {
+        var screenWidth = Waypoint.viewportWidth();
+        if ($('.slideshow').length && screenWidth > 800) {
+          if (direction === 'down') {
+            $('header').addClass('overlay');
+          }
+          else {
+            $('header').removeClass('overlay');
+          }
+        }
+      },
+      offset: 88
+    });
+  }
+  this.inPreview = (/\/admin\/design/.test(top.location.pathname));
+  if (this.inPreview) {
+    setTimeout(function () {
+      Waypoint.refreshAll();
+    }, 800);
+  }
+
+  searchForm = $('form.search');
+  if (searchForm.length) {
+    $('body').on('click', 'a[href=#search]', function (e) {
+      e.preventDefault();
+      searchForm.addClass('open');
+      if (!isIE()) {
+        searchForm.find('input[type=text]').focus()
+      }
+    });
+    $('body').on('click', '.close_search', function (e) {
+      e.preventDefault();
+      searchForm.removeClass('open');
+    });
+  }
+  mobileNav = $('.mobile_nav')
+  $('body').on('click', '.nav_trigger', function (e) {
+    e.preventDefault();
+    mobileNav.addClass('open');
+  });
+  $('select').change(function (e) {
+    e.preventDefault();
+    $(this).blur();
+  })
+  $('body').on('click', '.close_nav', function (e) {
+    e.preventDefault();
+    mobileNav.removeClass('open');
+  });
+
+  $('#cart input[id$=_qty]').blur(function(e) {
+    e.preventDefault();
+    $(this).closest('form').submit();
+  });
+
+  $('#cart .remove').click(function(e) {
+    e.preventDefault();
+    $(this).closest('li').find('input[id$=_qty]').val(0).closest('form').submit();
+  });
+
+});
+$(document).keyup(function (e) {
+  if (e.keyCode == 27) {
+    if (searchForm.length) {
+      searchForm.removeClass('open');
+    }
+    if (mobileNav.length) {
+      mobileNav.removeClass('open');
+    }
+  }
+});
+var isGreaterThanZero = function(currentValue) {
+  return currentValue > 0;
+}
+
+function arrayContainsArray(superset, subset) {
+  if (0 === subset.length) {
+    return false;
+  }
+  return subset.every(function (value) {
+    return (superset.indexOf(value) >= 0);
+  });
+}
+
+function unique(item, index, array) {
+  return array.indexOf(item) == index;
+}
+
+function cartesianProduct(a) {
+  var i, j, l, m, a1, o = [];
+  if (!a || a.length == 0) return a;
+  a1 = a.splice(0, 1)[0];
+  a = cartesianProduct(a);
+  for (i = 0, l = a1.length; i < l; i++) {
+    if (a && a.length) for (j = 0, m = a.length; j < m; j++)
+      o.push([a1[i]].concat(a[j]));
+    else
+      o.push([a1[i]]);
+  }
+  return o;
+}
+
+Array.prototype.equals = function (array) {
+  if (!array)
+    return false;
+  if (this.length != array.length)
+    return false;
+  for (var i = 0, l=this.length; i < l; i++) {
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      if (!this[i].equals(array[i]))
+        return false;
+    }
+    else if (this[i] != array[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// From https://github.com/kevlatus/polyfill-array-includes/blob/master/array-includes.js
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function (searchElement, fromIndex) {
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+      var o = Object(this);
+      var len = o.length >>> 0;
+      if (len === 0) {
+        return false;
+      }
+      var n = fromIndex | 0;
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+      while (k < len) {
+        if (sameValueZero(o[k], searchElement)) {
+          return true;
+        }
+        k++;
+      }
+      return false;
+    }
+  });
+}
+
+Array.prototype.count = function(filterMethod) {
+  return this.reduce((count, item) => filterMethod(item)? count + 1 : count, 0);
+}
+if ($('.product_option_select').length) {
+  disableAddButton();
+}
+$('.product_option_select').on('change',function() {
+  var option_price = $(this).find("option:selected").attr("data-price");
+  enableAddButton(option_price);
+});
+function enableAddButton(updated_price) {
+  var addButton = $('.add-to-cart-button');
+  var addButtonTitle = addButton.attr('data-add-title');
+  addButton.attr("disabled",false);
+  if (updated_price) {
+    priceTitle = ' - ' + Format.money(updated_price, true, true);
+  }
+  else {
+    priceTitle = '';
+  }
+  addButton.html(addButtonTitle + priceTitle);
+}
+
+function disableAddButton(type) {
+  var addButton = $('.add-to-cart-button');
+  var addButtonTitle = addButton.attr('data-add-title');
+  if (type == "sold-out") {
+    var addButtonTitle = addButton.attr('data-sold-title');
+  }
+  if (!addButton.is(":disabled")) {
+    addButton.attr("disabled","disabled");
+  }
+  addButton.html(addButtonTitle);
+}
+
+function enableSelectOption(select_option) {
+  select_option.removeAttr("disabled");
+  select_option.text(select_option.attr("data-name"));
+  select_option.removeAttr("disabled-type");
+}
+function disableSelectOption(select_option, type) {
+  if (type === "sold-out") {
+    disabled_text = select_option.parent().attr("data-sold-text");
+    disabled_type = "sold-out";
+  }
+  if (type === "unavailable") {
+    disabled_text = select_option.parent().attr("data-unavailable-text");
+    disabled_type = "unavailable";
+  }
+  if (select_option.val() > 0) {
+    var name = select_option.attr("data-name");
+    select_option.attr("disabled",true);
+    select_option.text(name + ' ' + disabled_text);
+    select_option.attr("disabled-type",disabled_type);
+  }
+}
